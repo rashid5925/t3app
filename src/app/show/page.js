5c22dbb676e355b21ecc6f96b1ee80b9c35c0b63
rashid5925 <rashid5925>
2024-06-06 20:27:12 +0500

    changes

--- src/app/show/page.js
+++ src/app/show/page.js
@@ -6,8 +6,12 @@ import { faVolumeXmark } from "@fortawesome/free-solid-svg-icons";
 import { faRecordVinyl } from "@fortawesome/free-solid-svg-icons";
 import { faPhotoFilm } from "@fortawesome/free-solid-svg-icons";
 import ChatAsk from "@/components/ChatAsk";
-import { useEffect, useState } from "react";
-import ExcaliCanvas from "@/components/excalidraw/ExcaliCanvas";
+import { useEffect, useState, useRef } from "react";
+import styles from "./VideoWithOverlay.module.css";
+
+// import ExcaliCanvas from "@/components/excalidraw/ExcaliCanvas";
+import dynamic from "next/dynamic";
+
 import { faRotateRight } from "@fortawesome/free-solid-svg-icons";
 import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
 import { faReply } from "@fortawesome/free-solid-svg-icons";
@@ -18,6 +22,10 @@ import { useUser } from "@/lib/auth";
 import Spinner from "@/components/Spinner";
 import { Separator } from "@/components/ui/separator";
 
+const ExcalidrawComponent = dynamic(
+  () => import("@excalidraw/excalidraw").then((mod) => mod.Excalidraw),
+  { ssr: false }
+);
 const Show = () => {
   const router = useSearchParams();
   const mode = router.get("threadid");
@@ -29,7 +37,16 @@ const Show = () => {
   const [play, setPlay] = useState(false);
   const rout = useRouter();
   const user = useUser();
-  
+
+  const excalidrawRef = useRef(null);
+
+  useEffect(() => {
+    // Focus on the Excalidraw component and enable pen by default
+    if (excalidrawRef.current) {
+      excalidrawRef.current.updateScene({ appState: { activeTool: 'pen' } });
+    }
+  }, []);
+
 
   useEffect(() => {
     let interval = null;
@@ -63,32 +80,32 @@ const Show = () => {
       <div className="flex flex-col gap-7 fixed top-10 right-10 z-50 bg-white rounded-lg p-2">
         {video && volume ? (
           <>
-          <Image
-            src={"/icons/volume.svg"}
-            width={35}
-            height={35}
-            alt="volume"
-            className="cursor-pointer"
-            onClick={() => setVolume(!volume)}
-          />
-          <Separator className="h-1 border-dashed border-black border-2 bg-transparent" />
+            <Image
+              src={"/icons/volume.svg"}
+              width={35}
+              height={35}
+              alt="volume"
+              className="cursor-pointer"
+              onClick={() => setVolume(!volume)}
+            />
+            <Separator className="h-1 border-dashed border-black border-2 bg-transparent" />
           </>
         ) : video ? (
           <>
-          <FontAwesomeIcon
-            onClick={() => setVolume(!volume)}
-            className="cursor-pointer"
-            style={{ width: "35px", height: "30px" }}
-            icon={faVolumeXmark}
-          />
-          <Separator className="h-1 border-dashed border-black border-2 bg-transparent" />
+            <FontAwesomeIcon
+              onClick={() => setVolume(!volume)}
+              className="cursor-pointer"
+              style={{ width: "35px", height: "30px" }}
+              icon={faVolumeXmark}
+            />
+            <Separator className="h-1 border-dashed border-black border-2 bg-transparent" />
           </>
         ) : (
           <></>
         )}
         {video ? (
           <>
-            <ChatAsk  postId={mode} />
+            <ChatAsk postId={mode} />
             <Separator className="h-1 border-dashed border-black border-2 bg-transparent" />
             <ChatPopover>
               <FontAwesomeIcon
@@ -133,27 +150,27 @@ const Show = () => {
         {!video && status == 0 ? (
           <>
             <Separator className="h-1 border-dashed border-black border-2 bg-transparent" />
-          <FontAwesomeIcon
-            icon={faRecordVinyl}
-            style={{ width: "35px", height: "30px" }}
-            className="cursor-pointer"
-            onClick={() => setStatus(1)}
-          />
+            <FontAwesomeIcon
+              icon={faRecordVinyl}
+              style={{ width: "35px", height: "30px" }}
+              className="cursor-pointer"
+              onClick={() => setStatus(1)}
+            />
           </>
         ) : !video && status == 1 ? (
           <>
             <Separator className="h-1 border-dashed border-black border-2 bg-transparent" />
-          <span className="countdown font-mono text-2xl">
-            <span
-              style={{
-                "--value": ("0" + Math.floor(timer % 60)).slice(-2),
-              }}
-            ></span>
-          </span>
+            <span className="countdown font-mono text-2xl">
+              <span
+                style={{
+                  "--value": ("0" + Math.floor(timer % 60)).slice(-2),
+                }}
+              ></span>
+            </span>
           </>
         ) : !video ? (
           <>
-          <Separator className="h-1 border-dashed border-black border-2 bg-transparent" />
+            <Separator className="h-1 border-dashed border-black border-2 bg-transparent" />
             <Image
               src={"/icons/screen.svg"}
               width={30}
@@ -199,7 +216,15 @@ const Show = () => {
             <source src="/video.mp4" />
           </video>
         ) : (
-          <ExcaliCanvas height="94vh" width="97vw" />
+          <div className={styles.container}>
+            <video className={styles.video} autoPlay muted loop>
+              <source src="/video.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            <div className={styles.overlay}>
+              <ExcalidrawComponent ref={excalidrawRef} />
+            </div>
+          </div>
         )}
       </div>
       {!image ? (
@@ -209,67 +234,70 @@ const Show = () => {
           } w-screen h-screen bottom-0 z-30`}
           style={{ pointerEvents: "none" }}
         >
-          {typeof window !== 'undefined' ? 
-          <Rnd
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              border: "solid 1px #ddd",
-              background: "#f0f0f0",
-              pointerEvents: "all",
-              borderRadius: "30px"
-            }}
-            default={{
-              x: 0,
-              y: window.innerHeight - 250,
-              width: 260,
-              height: 250,
-            }}
-          >
-            <div className="flex flex-col justify-around items-center w-full h-full">
-              <p>Draggable</p>
-              <div className="flex flex-col justify-around items-center w-full">
-                <div className="flex justify-around items-center py-3 w-full">
-                  <FontAwesomeIcon
-                    icon={faRotateLeft}
-                    style={{ width: "35px", height: "30px" }}
-                    className="cursor-pointer"
-                  />
-                  <Image
-                    src={`/icons/${play? "play": "pause"}.svg`}
-                    width={36}
-                    height={36}
-                    alt="camera"
-                    onClick={() => setPlay(!play)}
-                    className="cursor-pointer"
-                  />
-                  <FontAwesomeIcon
-                    icon={faRotateRight}
-                    style={{ width: "35px", height: "30px" }}
-                    className="cursor-pointer"
-                  />
-                </div>
-                <Separator className="h-0 border-dashed border-black border-2 bg-transparent" />
-                <div className="flex justify-around items-center py-3 w-full">
-                  <Image
-                    src={"/icons/left.svg"}
-                    width={36}
-                    height={36}
-                    alt="left"
-                    className="cursor-pointer"
-                  />
-                  <Image
-                    src={"/icons/right.svg"}
-                    width={36}
-                    height={36}
-                    alt="right"
-                    className="cursor-pointer"
-                  />
+          {typeof window !== "undefined" ? (
+            <Rnd
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                border: "solid 1px #ddd",
+                background: "#f0f0f0",
+                pointerEvents: "all",
+                borderRadius: "30px",
+              }}
+              default={{
+                x: 0,
+                y: window.innerHeight - 250,
+                width: 260,
+                height: 250,
+              }}
+            >
+              <div className="flex flex-col justify-around items-center w-full h-full">
+                <p>Draggable</p>
+                <div className="flex flex-col justify-around items-center w-full">
+                  <div className="flex justify-around items-center py-3 w-full">
+                    <FontAwesomeIcon
+                      icon={faRotateLeft}
+                      style={{ width: "35px", height: "30px" }}
+                      className="cursor-pointer"
+                    />
+                    <Image
+                      src={`/icons/${play ? "play" : "pause"}.svg`}
+                      width={36}
+                      height={36}
+                      alt="camera"
+                      onClick={() => setPlay(!play)}
+                      className="cursor-pointer"
+                    />
+                    <FontAwesomeIcon
+                      icon={faRotateRight}
+                      style={{ width: "35px", height: "30px" }}
+                      className="cursor-pointer"
+                    />
+                  </div>
+                  <Separator className="h-0 border-dashed border-black border-2 bg-transparent" />
+                  <div className="flex justify-around items-center py-3 w-full">
+                    <Image
+                      src={"/icons/left.svg"}
+                      width={36}
+                      height={36}
+                      alt="left"
+                      className="cursor-pointer"
+                    />
+                    <Image
+                      src={"/icons/right.svg"}
+                      width={36}
+                      height={36}
+                      alt="right"
+                      className="cursor-pointer"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          </Rnd> : <></>}
+            </Rnd>
+          ) : (
+            <></>
+          )}
         </div>
       ) : (
         <></>
